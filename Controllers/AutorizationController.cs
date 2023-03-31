09848dd17ead68a588ae746f1c6dcc51e577c9b3
Gnusarov Vlad <gnusarovvladislav@gmail.com>
2023-03-31 15:15:53 +0300

    Добавлены стили для шапки сайта

--- Controllers/AutorizationController.cs
+++ Controllers/AutorizationController.cs
@@ -22,6 +22,7 @@ namespace ForumSite.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public async Task<IActionResult> Autorization(LoginModel loginModel)
         {
